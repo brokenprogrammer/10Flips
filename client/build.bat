f3bbd22beef795101d2b6db7bf107948466e8098
Oskar <brokenprogrammer@gmail.com>
2021-10-21 19:44:35 +0200

    Minimal draw assets sample.

--- client/build.bat
+++ client/build.bat
@@ -3,6 +3,6 @@
 if not exist build mkdir build
 pushd build
 
-emcc ../tenflips.c -s WASM=1 -o tenflips.html
+emcc ../tenflips.c -O2 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' --preload-file assets/8BitDeckAssets.png --use-preload-plugins -o tenflips.html
 
 popd
--- client/tenflips.c
+++ client/tenflips.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <emscripten.h>
 
-int main()
+typedef struct
 {
-    printf("Hello World!\n");
+    SDL_Renderer *Renderer;
+    SDL_Texture *CardsTexture;
+    SDL_Rect CardsTextureSize;
+
+} GameState;
+
+void
+Update(void *Argument)
+{
+    GameState *State = Argument;
+
+    SDL_RenderClear(State->Renderer);
+    SDL_RenderCopy(State->Renderer, State->CardsTexture, NULL, &State->CardsTextureSize);
+    SDL_RenderPresent(State->Renderer);
+}
+
+int 
+main()
+{
+    SDL_Window *Window;
+    GameState State = {};
+
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+    SDL_CreateWindowAndRenderer(600, 400, 0, &Window, &State.Renderer);
+    SDL_SetRenderDrawColor(State.Renderer, 255, 255, 255, 255);
+
+    {
+        SDL_Surface *Image = IMG_Load("assets/8BitDeckAssets.png");
+        if (!Image)
+        {
+            printf("Failed to load image: %s\n", IMG_GetError());
+            return 0;
+        }
+
+        State.CardsTexture = SDL_CreateTextureFromSurface(State.Renderer, Image);
+        State.CardsTextureSize.w = Image->w;
+        State.CardsTextureSize.h = Image->h;
+
+        SDL_FreeSurface (Image);
+    }
+
+
+    // NOTE(Oskar): Schedule the main loop handler
+    emscripten_set_main_loop_arg(Update, &State, -1, 1);
+
+    return 0;
 }
