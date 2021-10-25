75eacffabff76cdedaf5dd966c8db156ecc8bce9
Jesper Bergström <jesper.b96@gmail.com>
2021-10-25 22:07:32 +0200

    Refactors parsing player messages to server

---
+++ server/10FlipServer/10FlipServer/Models/PlayerAction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _10FlipServer.Models
+{
+    public class PlayerAction
+    {
+        public static string[] TypeStrings = { "lobby", "create", "connect", "start", "place", "endturn", "pickup" };
+
+        public PlayerActionType Type;
+        public string[] Arguments;
+
+        public PlayerAction()
+        {
+            Arguments = new string[0];
+        }
+
+        public static PlayerAction ParsePlayerAction(string value)
+        {
+            PlayerAction action = new PlayerAction();
+
+            string[] parts = value.Split(":");
+
+            string typeStr = parts[0];
+            action.Type = (PlayerActionType) Array.IndexOf(TypeStrings, typeStr);
+            if (action.Type == PlayerActionType.Error)
+            {
+                return null;
+            }
+
+            if (parts.Length == 2)
+            {
+                action.Arguments = parts[1].Split(",");
+            }
+
+            return action;
+        }
+    }
+
+    public enum PlayerActionType
+    {
+        Error = -1,
+        Lobby = 0,
+        Create = 1,
+        Connect = 2,
+        Start = 3,
+        Place = 4,
+        EndTurn = 5,
+        PickUp = 6
+    }
+}
